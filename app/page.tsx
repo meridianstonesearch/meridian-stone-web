2ff2bd608753ab3735ea7d696305fbd9fcec2eb0
meridianstonesearch <ian@meridianstonesearch.com>
2026-04-07 18:22:12 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import SiteNav from "@/components/SiteNav";
 import React, { useEffect, useRef, useState } from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
