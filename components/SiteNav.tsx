d8ec5e1109fc0b2988379b078779133d38866ea7
meridianstonesearch <ian@meridianstonesearch.com>
2026-04-08 00:52:40 -0400

    Update SiteNav.tsx

--- components/SiteNav.tsx
+++ components/SiteNav.tsx
@@ -28,6 +28,13 @@ export default function SiteNav({ onConnect }: SiteNavProps) {
           href="/battery-energy-storage-recruiter"
           className="group relative text-[9px] md:text-[10px] font-black uppercase tracking-[0.2em] opacity-60 hover:opacity-100 transition-all duration-300 shrink-0"
         >
+          <a
+  href="/development-recruiter"
+  className="group relative text-[9px] md:text-[10px] font-black uppercase tracking-[0.2em] opacity-60 hover:opacity-100 transition-all duration-300 shrink-0"
+>
+  Dev
+  <span className="absolute left-0 -bottom-1 h-px w-0 bg-[#111111] transition-all duration-300 group-hover:w-full" />
+</a>
           BESS
           <span className="absolute left-0 -bottom-1 h-px w-0 bg-[#111111] transition-all duration-300 group-hover:w-full" />
         </a>
